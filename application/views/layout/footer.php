b8287bf8886450c452631b66ce50efc34f96a43c
Rick Schultz <rick-schultz@hotmail.com>
2021-07-02 17:05:09 -0300

    migrate website to codeigniter 3

---
+++ application/views/layout/footer.php
@@ -0,0 +1,10 @@
+<footer>
+ <div class="text-center">
+  <a href="#TOP"><img src="assets/images/arrow-up.svg" alt="Top Button" width="50px" /></a>
+ </div>
+ <br />
+</footer>
+</div>
+</body>
+
+</html>
