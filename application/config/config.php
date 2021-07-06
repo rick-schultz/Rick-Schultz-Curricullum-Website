954f0e62285e5e5f64120a291cf2d0aadd648f48
Rick Schultz <60229666+rick-schultz@users.noreply.github.com>
2021-07-06 18:18:28 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://cv.rick-schultz/';
+$config['base_url'] = 'cv.rick-schultz/';
 
 /*
 |--------------------------------------------------------------------------
